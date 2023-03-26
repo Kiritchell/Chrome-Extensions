776550575e7db1de6c5a8370fd9c216d3f475a41
Kiritchell <kiritchell@gmail.com>
2023-03-26 17:08:24 -0500

    --all

--- script.js
+++ script.js
@@ -2,18 +2,38 @@ const inputEl = document.querySelector('#input-el')
 const saveBTN = document.querySelector('#save-btn')
 let myLeads = []
 const ulEl = document.querySelector('#ul-el')
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("Lead"))
+
+console.log(leadsFromLocalStorage)
+
+// Get the leads from the localStorage - PS: JSON.parse()
+// Store it in a variable, leadsFromLocalStorage
+// Log out the variable
+
+//The two below will save with save button or enter key
+
+inputEl.addEventListener('keypress', function(event) {
+  if (event.keyCode === 13) {
+    myLeads.push(inputEl.value)
+    localStorage.setItem("Lead", JSON.stringify(myLeads))
+    renderLeads()
+    inputEl.value = ""
+  }
+});
 
 saveBTN.addEventListener('click', function(){
-  console.log(myLeads)
   myLeads.push(inputEl.value)
+  localStorage.setItem("Lead", JSON.stringify(myLeads  ))
   renderLeads()
   inputEl.value = ""
 })
 
+
+
 function renderLeads(){
   let listItems = ""
     for(let i = 0; i<myLeads.length;i++){
-    listItems += `<li>
+    listItems += `<li class="list-items">
                       <a target='_blank' href='https://www.${myLeads[i]}'>
                        ${myLeads[i]}
                       </a>
