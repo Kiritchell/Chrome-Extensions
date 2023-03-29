6baa6f1d3cda68ed862f608959ba83842f740522
Kiritchell <kiritchell@gmail.com>
2023-03-28 22:08:06 -0500

    --all

--- script.js
+++ script.js
@@ -5,52 +5,58 @@ const ulEl = document.querySelector('#ul-el')
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 const clearButton=document.querySelector('#clear-btn')
 const tabBtn = document.querySelector('#tab-btn')
-let tabs = [{url:"google.com"}]
 
 if (leadsFromLocalStorage) {
-  myLeads = leadsFromLocalStorage
-  render(myLeads)
+myLeads = leadsFromLocalStorage
+render(myLeads)
 }
 
-inputEl.addEventListener('keypress', function(event) {
-  if (event.keyCode === 13) {
-    myLeads.push(inputEl.value)
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    render(myLeads)
-    inputEl.value = ""
-  }
-});
 
-saveBTN.addEventListener('click', function(){
+
+inputEl.addEventListener('keypress', function(event) {
+if (event.keyCode === 13) {
   myLeads.push(inputEl.value)
   localStorage.setItem("myLeads", JSON.stringify(myLeads))
   render(myLeads)
   inputEl.value = ""
+}
+});
+
+saveBTN.addEventListener('click', function(){
+myLeads.push(inputEl.value)
+localStorage.setItem("myLeads", JSON.stringify(myLeads))
+render(myLeads)
+inputEl.value = ""
 })
 
 
 
 function render(leads) {
-  let listItems = ""
-  for (let i = 0; i < leads.length; i++) {
-      listItems += `
-          <li>
-              <a target='_blank' href='${leads[i]}'>
-                  ${leads[i]}
-              </a>
-          </li>
-      `
-  }
-  ulEl.innerHTML = listItems
+let listItems = ""
+for (let i = 0; i < leads.length; i++) {
+    listItems += `
+        <li>
+            <a target='_blank' href='${leads[i]}'>
+                ${leads[i]}
+            </a>
+        </li>
+    `
+}
+ulEl.innerHTML = listItems
 }
 
 clearButton.addEventListener('dblclick', function(){
-  localStorage.clear(myLeads)
-  myLeads = []
-  render(myLeads)
+localStorage.clear(myLeads)
+myLeads = []
+render(myLeads)
 })
 
 tabBtn.addEventListener('click', function(){
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+myLeads.push(tabs[0].url)
+localStorage.setItem("myLeads", JSON.stringify(myLeads))
+render(myLeads)
+})
 
 })
 
